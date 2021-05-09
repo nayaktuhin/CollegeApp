db774960b1df7c79cce3f672fd09ef51c711af11
nayaktuhin <nayaktuhin@yahoo.in>
2021-05-09 21:07:59 +0530

    After Android Studio 4.2 Update

--- app/src/main/java/salty/contaminated/collegeapp/LoginActivity.java
+++ app/src/main/java/salty/contaminated/collegeapp/LoginActivity.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
-
+    private FirebaseApp mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // System.out.println("Yo");
+        mAuth = FirebaseApp.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        
     }
 }
